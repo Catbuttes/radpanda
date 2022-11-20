289e0ef0e8fd80b28ed152fcb53cb78a2e57d085
Ed Brereton <ed@buttes.dev>
2022-11-20 18:01:16 +0000

    Randomly select the image

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"time"
 
 	mastodon "github.com/mattn/go-mastodon"
@@ -23,10 +24,12 @@ func main() {
 	config := Configuration{}
 	flag.StringVar(&config.ServerUrl, "server", "", "The Mastodon Server")
 	flag.StringVar(&config.AccessToken, "token", "", "the app access token")
-	flag.StringVar(&config.MessageText, "message", "Have a panda!", "The message to send with each toot")
+	flag.StringVar(&config.MessageText, "message", "Red Pandas are rad! Have a panda!", "The message to send with each toot")
 	flag.BoolVar(&config.OneShot, "one-shot", false, "Single shot message")
 	flag.IntVar(&config.RepeatEveryMinutes, "repeat-duration", 60, "How many minutes per cycle")
 	flag.Parse()
+
+	rand.Seed(time.Now().UnixNano())
 
 	client := mastodon.NewClient(&mastodon.Config{
 		Server:      config.ServerUrl,
@@ -35,32 +38,34 @@ func main() {
 
 	ctx := context.Background()
 
-	files, err := ioutil.ReadDir("img")
-	if err != nil {
-		log.Panic(err)
-	}
-
-	attachment, err := client.UploadMedia(ctx, "img/"+files[10].Name())
-	if err != nil {
-		log.Printf("Error Uploading Media, %s\n", files[10].Name())
-		log.Fatal(err)
-	}
-
-	toot := mastodon.Toot{
-		Status:     config.MessageText,
-		Visibility: "private",
-		MediaIDs: []mastodon.ID{
-			attachment.ID,
-		},
-	}
-
-	_, err = client.PostStatus(ctx, &toot)
-	if err != nil {
-		log.Println("Error Tooting")
-		log.Fatal(err)
-	}
-
 	for {
+
+		files, err := ioutil.ReadDir("img")
+		if err != nil {
+			log.Panic(err)
+		}
+
+		fileIdx := rand.Intn(len(files)+1) + 1
+
+		attachment, err := client.UploadMedia(ctx, "img/"+files[fileIdx].Name())
+		if err != nil {
+			log.Printf("Error Uploading Media, %s\n", files[fileIdx].Name())
+			log.Fatal(err)
+		}
+
+		toot := mastodon.Toot{
+			Status:     config.MessageText,
+			Visibility: "private",
+			MediaIDs: []mastodon.ID{
+				attachment.ID,
+			},
+		}
+
+		_, err = client.PostStatus(ctx, &toot)
+		if err != nil {
+			log.Println("Error Tooting")
+			log.Fatal(err)
+		}
 
 		if config.OneShot {
 			return
